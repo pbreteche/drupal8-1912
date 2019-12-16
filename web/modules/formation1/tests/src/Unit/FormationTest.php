897e994d14f0a328120e47c48e233477e90ca3a6
Pierre Bretéché <pbreteche@dawan.fr>
2019-12-16 10:31:48 +0100

    first dummy unit test

---
+++ web/modules/formation1/tests/src/Unit/FormationTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\Tests\formation1\Unit;
+
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Test description.
+ *
+ * @group formation1
+ */
+class FormationTest extends UnitTestCase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    // @TODO: Mock required classes here.
+  }
+
+  /**
+   * Tests something.
+   */
+  public function testSomething() {
+    $this->assertTrue(TRUE, 'This is TRUE!');
+  }
+
+}
