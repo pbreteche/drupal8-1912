7d02d17d6bf0b60f1d2122187230c7928ceb12c7
Pierre Bretéché <pbreteche@dawan.fr>
2019-12-16 12:14:48 +0100

    rewrite unit test with prophecy

--- web/modules/formation1/tests/src/Unit/Example2Test.php
+++ web/modules/formation1/tests/src/Unit/Example2Test.php
@@ -19,6 +19,11 @@ class Example2Test extends UnitTestCase {
   private $example2;
 
   /**
+   * @var \Drupal\formation1\Example2
+   */
+  private $example2bis;
+
+  /**
    * {@inheritdoc}
    */
   protected function setUp() {
@@ -29,6 +34,11 @@ class Example2Test extends UnitTestCase {
       ->with(5)
       ->willReturn(25);
     $this->example2 = new Example2($exampleMock);
+
+    $prophecy = $this->prophesize(Example::class);
+    $prophecy->square(5)->willReturn(25)->shouldBeCalledTimes(1);
+
+    $this->example2bis = new Example2($prophecy->reveal());
   }
 
   /**
@@ -36,6 +46,9 @@ class Example2Test extends UnitTestCase {
    */
   public function testSomething() {
     $result = $this->example2->doSomething(5);
+    $this->assertSame(50, $result);
+
+    $result = $this->example2bis->doSomething(5);
     $this->assertSame(50, $result);
   }
 
